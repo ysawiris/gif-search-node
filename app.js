4384ba2f18f74bb84dfa7ddb9773135b18f6ed34
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-01-22 15:25:41 -0800

    first route updated

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-});
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+})
 
 // Start Server
 
